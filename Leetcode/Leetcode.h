c91b504754ed0df2fd3073920308bbe38f84f0b8
pashaMoroz <vferme2015@gmail.com>
2022-11-25 13:14:30 -0500

    Initial Commit

---
+++ Leetcode/Leetcode.h
@@ -0,0 +1,18 @@
+//
+//  Leetcode.h
+//  Leetcode
+//
+//  Created by Admin on 25/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Leetcode.
+FOUNDATION_EXPORT double LeetcodeVersionNumber;
+
+//! Project version string for Leetcode.
+FOUNDATION_EXPORT const unsigned char LeetcodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Leetcode/PublicHeader.h>
+
+
